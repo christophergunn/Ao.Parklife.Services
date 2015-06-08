b4448c7a11b1d15a268496e088fbcc604da18cb0
Rajitha Edirisinghe <rajitha.ed@gmail.com>
2015-06-08 11:06:56 +0100

    added a simple end point

---
+++ Ao.Parklife.Services/Ao.Parklife.Services/Controllers/ParkLifeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Ao.Parklife.Services.Models;
+
+namespace Ao.Parklife.Services.Controllers
+{
+    public class ParkLifeController : ApiController
+    {
+        private readonly Users[] Users =
+        {
+            new Users {Id = 1, Name = "Chris"},
+            new Users {Id = 2, Name = "Raj"},
+            new Users {Id = 3, Name = "Vince"}
+        };
+
+        public Users[] GetAllUsers()
+        {
+            return Users;
+        }
+    }
+}
