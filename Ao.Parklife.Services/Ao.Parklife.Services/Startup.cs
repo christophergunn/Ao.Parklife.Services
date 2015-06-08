f6b488d11ceb0e707de9bba60b5da7939315b7e6
Christopher Gunn <christopher.gunn@drllimited.co.uk>
2015-06-08 19:48:06 +0100

    Owin cors

--- Ao.Parklife.Services/Ao.Parklife.Services/Startup.cs
+++ Ao.Parklife.Services/Ao.Parklife.Services/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Owin;
 using Microsoft.Owin;
 [assembly: OwinStartup(typeof(SignalRChat.Startup))]
 namespace SignalRChat
@@ -7,8 +9,18 @@ namespace SignalRChat
     {
         public void Configuration(IAppBuilder app)
         {
-            // Any connection or hub wire up and configuration should go here
-            app.MapSignalR();
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+
+                var hubConfiguration = new HubConfiguration
+                {
+                    EnableDetailedErrors = true,
+                    EnableJSONP = true
+                };
+
+                map.RunSignalR(hubConfiguration);
+            });
         }
     }
 }
